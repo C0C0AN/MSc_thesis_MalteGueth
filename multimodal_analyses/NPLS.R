7b1986b55951bf47fe2f87cd8fdd03c1fd565a81
Malte Rudo Güth <maltegueth@gmail.com>
2018-03-27 07:27:19 +0200

    Update NPLS.R

--- multimodal_analyses/NPLS.R
+++ multimodal_analyses/NPLS.R
@@ -12,3 +12,11 @@ Y_npls<-matrix(2+0.4*X_npls[,5,1]+0.7*X_npls[,10,1]-0.9*X_npls[,15,1]+
                  0.6*X_npls[,20,1]- 0.5*X_npls[,25,1]+rnorm(50), ncol=1)
 
 fit<-sNPLS(X_npls, Y_npls, ncomp=3, keepJ = rep(2,3) , keepK = rep(1,3))
+
+# Out:
+# Component number  1 
+# Number of iterations:  1 
+# Component number  2 
+# Number of iterations:  1 
+# Component number  3 
+# Number of iterations:  1 
