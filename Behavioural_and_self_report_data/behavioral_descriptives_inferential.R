8943cbe427416fd76258d61dd15dc50e2b754e2d
Malte Rudo Güth <maltegueth@gmail.com>
2018-03-29 08:14:30 +0200

    Update behavioral_descriptives_inferential.R

--- Behavioural_and_self_report_data/behavioral_descriptives_inferential.R
+++ Behavioural_and_self_report_data/behavioral_descriptives_inferential.R
@@ -6,7 +6,7 @@
 # Get some basic descriptive and inferential statistics
 
 # This section on reading data and creating tables, starting at this point has been written by José Alanis
-# for usage in the section of neuropsychology
+# for usage in the section of neuropsychology (https://github.com/JoseAlanis)
 #############################################
 
 setwd('/Volumes/INTENSO/DPX_EEG_fMRI/Behavioral_Data/')
@@ -20,11 +20,11 @@ rt_data <- dplyr::rename(rt_data,
                          ID = .id, 
                          Block = V2, 
                          Trial = V4, 
-                         Trial_Type = V6, 
+                         trialtype = V6, 
                          RT = V10, 
                          Reaction = V12)
 
-rt_data <- dplyr::select(rt_data, ID, Block, Trial, Trial_Type, Reaction, RT)
+rt_data <- dplyr::select(rt_data, ID, Block, Trial, trialtype, Reaction, RT)
 
 rt_data$ID <- as.factor(rt_data$ID)
 rt_data <- dplyr::arrange(rt_data, ID)
@@ -33,14 +33,41 @@ rt_data$Subject <- as.factor(rt_data$Subject)
 
 rt_data$Reaction <- plyr::revalue(rt_data$Reaction, c(' hit' = 'correct', ' incorrect'='incorrect', ' miss'='too slow'))
 rt_corrects <- dplyr::filter(rt_data, Reaction == 'correct')
-rt_corrects$Trial_Type <- as.factor(rt_corrects$Trial_Type)
+rt_corrects$trialtype <- as.factor(rt_corrects$trialtype)
 
-rt_corrects_average <- plyr::ddply(rt_corrects, c('Trial_Type'), dplyr::summarise,
-                                Sum    = sum(!is.na(RT)),
-                                Mean_RT = mean(RT),
-                                sd   = sd(RT),
-                                se   = sd / sqrt(Sum),
-                                ci   = se * qt(.95/2 + .5, Sum-1)
+rt_corrects_average1 <- plyr::ddply(rt_corrects, c('trialtype'), dplyr::summarise,
+                                   Sum    = sum(!is.na(RT)),
+                                   Mean_RT = mean(RT),
+                                   sd   = sd(RT),
+                                   se   = sd / sqrt(Sum),
+                                   ci   = se * qt(.95/2 + .5, Sum-1)
+)
+
+rt_corrects_average2 <- plyr::ddply(rt_corrects, c('trialtype', 'Block'), dplyr::summarise,
+                                    Sum    = sum(!is.na(RT)),
+                                    Mean_RT = mean(RT),
+                                    sd   = sd(RT),
+                                    se   = sd / sqrt(Sum),
+                                    ci   = se * qt(.95/2 + .5, Sum-1)
 )
 
 #############################################
+
+# Test multiple models to check that the different trialtypes or blocks have a significant effect
+# and create bar chart with standard error
+
+MLnoIntercept <- glm(RT ~ Block + trialtype, data = rt_corrects) # ML model
+print(MLnoIntercept)
+anova(MLnoIntercept, test = "F")
+
+ML <- glm(RT ~ Block + trialtype:Block, data = rt_corrects)
+anova(ML, test = "F")
+
+require(ggplot2)
+require(viridis)
+ggplot(rt_corrects_average2, aes(x = Block, y = Mean_RT, fill = trialtype),
+       condition = trialtype) + 
+  geom_bar(position = position_dodge(), stat = 'identity') +
+  geom_errorbar(aes(ymin=Mean_RT - se, ymax=Mean_RT+se), width=.2, position = position_dodge(.9)) +
+  scale_fill_viridis(option = 'B', discrete = T, begin = 0.15, end = .85) +
+  coord_cartesian(ylim = c(300, 550))
