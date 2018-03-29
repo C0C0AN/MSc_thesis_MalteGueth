1c7d5c1bbca4768a037afdb0d78794755cc2ff95
Malte Rudo Güth <maltegueth@gmail.com>
2018-03-29 16:44:11 +0200

    Update behavioral_descriptives_inferential.R

--- Behavioural_and_self_report_data/behavioral_descriptives_inferential.R
+++ Behavioural_and_self_report_data/behavioral_descriptives_inferential.R
@@ -35,7 +35,7 @@ rt_data$Reaction <- plyr::revalue(rt_data$Reaction, c(' hit' = 'correct', ' inco
 rt_corrects <- dplyr::filter(rt_data, Reaction == 'correct')
 rt_corrects$trialtype <- as.factor(rt_corrects$trialtype)
 
-rt_corrects_average1 <- plyr::ddply(rt_corrects, c('trialtype'), dplyr::summarise,
+rt_corrects_average <- plyr::ddply(rt_corrects, c('trialtype'), dplyr::summarise,
                                    Sum    = sum(!is.na(RT)),
                                    Mean_RT = mean(RT),
                                    sd   = sd(RT),
@@ -43,7 +43,7 @@ rt_corrects_average1 <- plyr::ddply(rt_corrects, c('trialtype'), dplyr::summaris
                                    ci   = se * qt(.95/2 + .5, Sum-1)
 )
 
-rt_corrects_average2 <- plyr::ddply(rt_corrects, c('trialtype', 'Block'), dplyr::summarise,
+rt_corrects_average_blocks <- plyr::ddply(rt_corrects, c('trialtype', 'Block'), dplyr::summarise,
                                     Sum    = sum(!is.na(RT)),
                                     Mean_RT = mean(RT),
                                     sd   = sd(RT),
@@ -53,8 +53,25 @@ rt_corrects_average2 <- plyr::ddply(rt_corrects, c('trialtype', 'Block'), dplyr:
 
 #############################################
 
-# Test multiple models to check that the different trialtypes or blocks have a significant effect
-# and create bar chart with standard error
+# Build new variable in averaged data frame for the proactive behavioral shift index (PSI)
+
+rt_corrects_average_blocks$PSI <- (rt_corrects_average_blocks$Mean_RT[rt_corrects_average_blocks$trialtype==3] 
+                             - rt_corrects_average_blocks$Mean_RT[rt_corrects_average_blocks$trialtype==2])/
+                            (rt_corrects_average_blocks$Mean_RT[rt_corrects_average_blocks$trialtype==3] 
+                             + rt_corrects_average_blocks$Mean_RT[rt_corrects_average_blocks$trialtype==2])
+
+# Build new data frame for errors and error rates by block and trialtype
+
+incorrects <- dplyr::filter(rt_data, Reaction == 'incorrect')
+incorrects$trialtype <- as.factor(incorrects$trialtype)
+
+incorrects_average_blocks <- plyr::ddply(incorrects, c('trialtype', 'Block'), dplyr::summarise,
+                                          Sum    = sum(!is.na(Reaction=='incorrect')),
+                                          Mean = mean(Sum),
+                                          sd   = sd(Sum),
+                                          se   = sd / sqrt(Sum),
+                                          ci   = se * qt(.95/2 + .5, Sum-1)
+)
 
 MLnoIntercept <- glm(RT ~ Block + trialtype, data = rt_corrects) # ML model
 print(MLnoIntercept)
@@ -63,11 +80,16 @@ anova(MLnoIntercept, test = "F")
 ML <- glm(RT ~ Block + trialtype:Block, data = rt_corrects)
 anova(ML, test = "F")
 
+
+
 require(ggplot2)
 require(viridis)
-ggplot(rt_corrects_average2, aes(x = Block, y = Mean_RT, fill = trialtype),
+ggplot(rt_corrects_average_blocks, aes(x = Block, y = Mean_RT, fill = trialtype),
        condition = trialtype) + 
   geom_bar(position = position_dodge(), stat = 'identity') +
   geom_errorbar(aes(ymin=Mean_RT - se, ymax=Mean_RT+se), width=.2, position = position_dodge(.9)) +
-  scale_fill_viridis(option = 'B', discrete = T, begin = 0.15, end = .85) +
-  coord_cartesian(ylim = c(300, 550))
+  scale_fill_viridis(option = 'B', discrete = T, begin = 0.15, end = .85, 
+                     labels=c("AX", "BX", "AY","BY")) +
+  coord_cartesian(ylim = c(300, 550)) +
+  labs(x = "block", y = "reaction times [ms]") +
+  theme_classic()
