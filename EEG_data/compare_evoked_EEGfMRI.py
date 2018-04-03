308561c66b974aaf6fb184de15bd8451b26eac86
Malte Rudo Güth <maltegueth@gmail.com>
2018-04-03 17:36:53 +0200

    Update compare_evoked_EEGfMRI.py

--- EEG_data/compare_evoked_EEGfMRI.py
+++ EEG_data/compare_evoked_EEGfMRI.py
@@ -11,10 +11,28 @@ Created on Fri Dec 06 05:56:37 2017
 # Appnending them to create a list, enables plotting confindence intervals or standradr errors
 # as shaded areas around the line plots
 
-
 import mne
+
 from matplotlib.backends.backend_pdf import PdfPages
 
+# Build the digital montage of your eeg system
+# It's also possible to specifiy a path to a montage file
+# with ‘.elc’, ‘.txt’, ‘.csd’, ‘.elp’, ‘.hpts’, ‘.sfp’, ‘.loc’ 
+# (‘.locs’ and ‘.eloc’) or .bvef as supported data formats
+
+montage = mne.channels.read_montage(kind='standard_1005')
+s_freq = 250
+chanlabels = ['Fp1',	'Fp2',	'F3',	'F4',	'C3',	'C4',	'P3',	'P4',	
+              'O1',	'O2',	'F7',	'F8',	'T7',	'T8',	'P7',	'P8',	
+              'Fz',	'Cz',	'Pz',	'Oz',	'FC1',	'FC2',	'CP1',	'CP2',	
+              'FC5',	'FC6',	'CP5',	'CP6',	'TP9',	'TP10', 'POz']
+ch_types = ['eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	
+            'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	
+            'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	
+            'eeg',	'eeg',	'eeg',	'eeg',	'eeg',	'eeg', 'eeg']
+montage = mne.channels.read_montage(kind='standard_1005')
+info_custom = mne.create_info(chanlabels, s_freq, ch_types, montage=montage)
+info_custom['description'] = 'Simultaneously recorded data with customized info file'
 
 listA = [evokedA_1, evokedA_2, evokedA_3, evokedA_4, evokedA_5, evokedA_6, 
          evokedA_7, evokedA_8, evokedA_9,evokedA_10, evokedA_11, evokedA_12, evokedA_13]
@@ -34,56 +52,49 @@ listAY = [evokedAY_1, evokedAY_2, evokedAY_3, evokedAY_4, evokedAY_5, evokedAY_6
 listBY = [evokedBY_1, evokedBY_2, evokedBY_3, evokedBY_4, evokedBY_5, evokedBY_6, 
           evokedBY_7, evokedBY_8, evokedBY_9,evokedBY_10, evokedBY_11, evokedBY_12, evokedBY_13]
 
-colors1 = dict(CueA="cyan", CueB="darkblue")
+colors1 = dict(CueA="CornFlowerBlue", CueB="darkblue")
+colors2 = dict(AX="lightcoral", BX="crimson",
+               AY="lightcoral", BY="crimson",)
+
 linestyles1 = dict(CueA='-', CueB='--')
+linestyles2 = dict(AX='-', BX='--',
+                   AY='-', BY='--')
+
 picks1 = ch_names.index('Pz')
-evoked_dict1 = {'CueA': lisA,'CueB': listB}
-
-evoked_cues_pz = mne.viz.plot_compare_evokeds(evoked_dict1, picks=picks1, truncate_yaxis=False, 
-                             title='Cue A (light blue) and Cue B (dark blue) at Pz averaged over all blocks',
-                             invert_y=True, ci=0.90, linestyles=linestyles, 
-                             colors=colors, ylim = dict(eeg=[-4,8]))
-
-
-colors2 = dict(AX="lightcoral", BX="crimson",
-               AY="lightcoral", BY="crimson",)
-linestyles2 = dict(AX='-', BX='--',
-                   AY='-', BY='--')
 picks2 = ch_names.index('Fz')
+
+evoked_dict1 = {'CueA': lisA,'CueB': listB}
 evoked_dict2 = {'AX': listAX, 'BX': listBX, 'AY': listAY, 'BY': listBY}
 
-evoked_probes_pz = mne.viz.plot_compare_evokeds(evoked_dict2, picks=picks2, truncate_yaxis=False, 
-                             title='AX (light blue, solid), BX (red, solid), AY (light blue, dashed) and BY (red, dashed) probe-locked at Pz over all blocks',
+vlines = list([0])
+
+evoked_cues_pz = mne.viz.plot_compare_evokeds(evoked_dict1, picks=picks1, truncate_yaxis='max_ticks', 
+                             truncate_xaxis='max_ticks', show_legend=True, title=None,
+                             invert_y=True, ci=0.90, linestyles=linestyles1, 
+                             colors=colors1, ylim = dict(eeg=[-4,8.5]), vlines=vlines)
+
+evoked_cues_fz = mne.viz.plot_compare_evokeds(evoked_dict1, picks=picks2, truncate_yaxis='max_ticks', 
+                             truncate_xaxis='max_ticks', show_legend=True, title=None,
+                             invert_y=True, ci=0.90, linestyles=linestyles1, 
+                             colors=colors1, ylim = dict(eeg=[-4,6]), vlines=vlines)
+
+evoked_probes_pz = mne.viz.plot_compare_evokeds(evoked_dict2, picks=picks1, truncate_yaxis='max_ticks', 
+                             truncate_xaxis='max_ticks', show_legend=True, title=None,
                              invert_y=True, ci=0.90, linestyles=linestyles2, 
-                             colors=colors2, ylim = dict(eeg=[-4,10]))
+                             colors=colors2, ylim = dict(eeg=[-4,10]), vlines=vlines)
 
-colors1 = dict(CueA="cyan", CueB="darkblue")
-linestyles1 = dict(CueA='-', CueB='--')
-picks1 = ch_names.index('Fz')
-evoked_dict1 = {'CueA': lisA,'CueB': listB}
-
-evoked_cues_fz = mne.viz.plot_compare_evokeds(evoked_dict1, picks=picks1, truncate_yaxis=False, 
-                             title='Cue A (light blue) and Cue B (dark blue) at Fz averaged over all blocks',
-                             invert_y=True, ci=0.90, linestyles=linestyles, 
-                             colors=colors, ylim = dict(eeg=[-4,8]))
-
-
-colors2 = dict(AX="lightcoral", BX="crimson",
-               AY="lightcoral", BY="crimson",)
-linestyles2 = dict(AX='-', BX='--',
-                   AY='-', BY='--')
-picks2 = ch_names.index('Fz')
-evoked_dict2 = {'AX': listAX, 'BX': listBX, 'AY': listAY, 'BY': listBY}
-
-evoked_probes_fz = mne.viz.plot_compare_evokeds(evoked_dict2, picks=picks2, truncate_yaxis=False, 
-                             title='AX (light blue, solid), BX (red, solid), AY (light blue, dashed) and BY (red, dashed) probe-locked at Pz over all blocks',
+evoked_probes_fz = mne.viz.plot_compare_evokeds(evoked_dict2, picks=picks2, truncate_yaxis='max_ticks', 
+                             truncate_xaxis='max_ticks', show_legend=True, title=None,
                              invert_y=True, ci=0.90, linestyles=linestyles2, 
-                             colors=colors2, ylim = dict(eeg=[-4,10]))
+                             colors=colors2, ylim = dict(eeg=[-4,10]), vlines=vlines)
 
 
-pp = PdfPages('compare_cues_probes_allblocks.pdf')
+pp = PdfPages('compare_cues_EEGfMRI.pdf')
 pp.savefig(evoked_cues_pz)
-pp.savefig(evoked_probes_pz)
 pp.savefig(evoked_cues_fz)
-pp.savefig(evoked_probes_fz)
+pp.close()
+
+pp = PdfPages('compare_probes_EEGfMRI.pdf')
+pp.savefig(evoked_cues_pz)
+pp.savefig(evoked_cues_fz)
 pp.close()
